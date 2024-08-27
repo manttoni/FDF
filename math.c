289aad99bd97f5c7cb557a187a73d2766af5cb41
Anttoni Maula <amaula@c1r6p8.hive.fi>
2024-08-27 13:07:29 +0300

    color gradient

--- math.c
+++ math.c
@@ -6,7 +6,7 @@
 /*   By: amaula <amaula@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 13:23:13 by amaula            #+#    #+#             */
-/*   Updated: 2024/08/27 12:46:39 by amaula           ###   ########.fr       */
+/*   Updated: 2024/08/27 13:07:10 by amaula           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ void	rotate(t_data *data, t_coord *coord)
 
 int	get_colour(t_grid *grid, t_coord current, t_coord end, int len)
 {
-	int	drawn;
+	int	undrawn;
 	int start_colour;
 	int	end_colour;
 	int	pixel_colour;
@@ -58,7 +58,7 @@ int	get_colour(t_grid *grid, t_coord current, t_coord end, int len)
 		return (0);
 	start_colour = 255 - 255 * current.z / grid->depth;
 	end_colour = 255 - 255 * end.z / grid->depth;
-	drawn = len - hypot(abs(current.x - end.x), abs(current.y - end.y));
-	pixel_colour = 255 - start_colour - (drawn * (end_colour - start_colour) / len); 
+	undrawn = hypot(abs(current.x - end.x), abs(current.y - end.y));
+	pixel_colour = start_colour - (undrawn * (end_colour - start_colour) / len); 
 	return (255 << 16 | pixel_colour << 8 | pixel_colour);
 }
